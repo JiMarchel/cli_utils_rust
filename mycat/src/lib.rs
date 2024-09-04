9944a601c9f932b0f971b70d7becbe09b5757393
JiMarchel <jmarchel100@gmail.com>
2024-09-04 21:22:21 +0700

    update mycat

--- mycat/src/lib.rs
+++ mycat/src/lib.rs
@@ -3,16 +3,48 @@ use std::{
     io::{self, Read},
 };
 
-pub fn cat(file_name: &str) -> io::Result<()> {
-    //Open file!
-    let mut file = File::open(file_name)?;
+pub struct Config {
+    pub file_name: String,
+    pub number: bool,
+    pub number_nonblank: bool,
+    pub show_ends: bool,
+}
 
-    //Read file
+impl Config {
+    pub fn number(contents: &str) {
+        for (i, line) in contents.lines().enumerate() {
+            println!("{:>6}  {}", i + 1, line)
+        }
+    }
+
+    pub fn number_nonblank(contents: &str) {
+        for (i, line) in contents.lines().enumerate() {
+            if line.trim().is_empty() {
+                println!("{}", line)
+            } else {
+                println!("{:>6} {}", i + 1, line)
+            }
+        }
+    }
+
+    pub fn show_ends(contents: &str) {
+        for line in contents.lines() {
+            println!("{}$", line)
+        }
+    }
+}
+
+pub fn cat(config: Config) -> io::Result<()> {
+    let mut file = File::open(config.file_name)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    //Display file content to terminal
-    println!("{}", contents);
+    match (config.number, config.number_nonblank, config.show_ends) {
+        (true, false, false) => Config::number(&contents),
+        (false, true, false) => Config::number_nonblank(&contents),
+        (false, false, true) => Config::show_ends(&contents),
+        (_, _, _) => println!("{}", contents),
+    }
 
     Ok(())
 }
--- mycat/src/main.rs
+++ mycat/src/main.rs
@@ -1,25 +1,43 @@
-use clap::{Arg, Command};
+use clap::{Arg, ArgAction, Command};
+use mycat::Config;
 use std::io;
 
 fn main() -> io::Result<()> {
     let cfg = Command::new("mycat")
-        .arg(
-            Arg::new("file")
-                .help("File to display")
-                .required(true)
-                .index(1),
-        )
+        .arg(Arg::new("file").help("File to display").required(true))
         .arg(
             Arg::new("number")
                 .short('n')
                 .long("number")
-                .help("number all output lines"),
+                .help("number all output lines")
+                .conflicts_with_all(["number-nonblank", "show-ends"])
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("number-nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("number nonempty output lines, overrides -n")
+                .conflicts_with("show-ends")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("show-ends")
+                .short('E')
+                .long("show-ends")
+                .help("display $ at end of each line")
+                .action(ArgAction::SetTrue),
         )
         .get_matches();
 
-    let file_name: &str = cfg.get_one::<String>("file").unwrap();
+    let config = Config {
+        file_name: cfg.get_one::<String>("file").unwrap().clone(),
+        number: cfg.get_flag("number"),
+        number_nonblank: cfg.get_flag("number-nonblank"),
+        show_ends: cfg.get_flag("show-ends"),
+    };
 
-    if let Err(e) = mycat::cat(file_name) {
+    if let Err(e) = mycat::cat(config) {
         eprintln!("Error reading file : {}", e)
     }
 
